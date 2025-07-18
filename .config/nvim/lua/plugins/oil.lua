d50b9beb779eb78186c788420f8a0eef4f428de1
aw <asward2@gmail.com>
2025-07-18 10:03:57 -0700

    q to close

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,19 +1,22 @@
 return {
-  'stevearc/oil.nvim',
-  ---@module 'oil'
-  ---@type oil.SetupOpts
-  opts = {},
-  -- Optional dependencies
-  dependencies = { { "echasnovski/mini.icons", opts = {} } },
-  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
-  lazy = false,
-  config = function()
-	require("oil").setup({
-		view_options = {
-			show_hidden = true,
-		}
-	})
-	vim.keymap.set('n', '<leader>e', '<Cmd>Oil --float<CR>')
+	'stevearc/oil.nvim',
+	---@module 'oil'
+	---@type oil.SetupOpts
+	opts = {},
+	-- Optional dependencies
+	dependencies = { { "echasnovski/mini.icons", opts = {} } },
+	-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+	-- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+	lazy = false,
+	config = function()
+		require("oil").setup({
+			view_options = {
+				show_hidden = true,
+			},
+			keymaps = {
+				["q"] = "actions.close",
+			},
+		})
+		vim.keymap.set('n', '<leader>e', '<Cmd>Oil --float<CR>')
 	end,
 }
