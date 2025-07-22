b195a7a2cf5aacec28b4f8837a7717d87b14745c
aw <asward2@gmail.com>
2025-07-22 10:22:47 -0700

    code companion fixes and autocomplete

--- .config/nvim/lua/plugins/code-companion.lua
+++ .config/nvim/lua/plugins/code-companion.lua
@@ -1,50 +1,37 @@
-local api_key = vim.env.ANTHROPIC_API_KEY or ""
-
-
 return {
   "olimorris/codecompanion.nvim",
-  tag = "v17.8.0",
+  tag = "v17.10.0",
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-treesitter/nvim-treesitter",
   },
-  config = function()
-    require("codecompanion").setup({
-      strategies = {
-        chat = {
-          adapter = "anthropic",
-        },
-        inline = {
-          adapter = "anthropic",
-        },
-        agent = {
-          adapter = "anthropic",
+  opts = {
+    strategies = {
+      cmd = {
+        adapter = "anthropic",
+      },
+      chat = {
+        adapter = "anthropic",
+        opts = {
+          completion_provider = "cmp",
         },
       },
-      display = {
-	chat = { show_settings = true },
-		},
-      adapters = {
-        anthropic = function()
-          return require("codecompanion.adapters").extend("anthropic", {
-            env = {
-              api_key = vim.env.ANTHROPIC_API_KEY or "",
-            },
-            schema = {
-              model = {
-                default = "claude-3-5-sonnet-latest",
-              },
-            },
-          })
-        end,
+      inline = {
+        adapter = "anthropic",
       },
-    })
-  end,
-  
-  -- -- Key mappings
-  -- keys = {
-  --   { "<C-a>", "<cmd>CodeCompanionActions<cr>", mode = { "n", "v" }, desc = "Code Companion Actions" },
-  --   { "<Leader>a", "<cmd>CodeCompanionChat Toggle<cr>", mode = { "n", "v" }, desc = "Toggle Code Companion Chat" },
-  --   { "ga", "<cmd>CodeCompanionChat Add<cr>", mode = "v", desc = "Add to Code Companion Chat" },
-  -- },
+      agent = {
+        adapter = "anthropic",
+      },
+    },
+    display = {
+      chat = { show_settings = true },
+    },
+  },
 }
+
+-- -- Key mappings
+-- keys = {
+--   { "<C-a>", "<cmd>CodeCompanionActions<cr>", mode = { "n", "v" }, desc = "Code Companion Actions" },
+--   { "<Leader>a", "<cmd>CodeCompanionChat Toggle<cr>", mode = { "n", "v" }, desc = "Toggle Code Companion Chat" },
+--   { "ga", "<cmd>CodeCompanionChat Add<cr>", mode = "v", desc = "Add to Code Companion Chat" },
+-- },
