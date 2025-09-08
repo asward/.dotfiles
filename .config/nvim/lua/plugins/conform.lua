492b36872cd55e72223946944cbaea10dbbc5412
aw <asward2@gmail.com>
2025-09-08 10:32:27 -0700

    lighter diag text

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -7,9 +7,37 @@ return {
 			timeout_ms = 500,
 		},
 		formatters_by_ft = {
-			python = { "black" }, -- Example: Use Black for Python
-			lua = { "stylua" }, -- Example: Use Stylua for Lua
-			-- Add other filetypes and their respective formatters
+			-- Web development
+			javascript = { "prettier" },
+			typescript = { "prettier" },
+			javascriptreact = { "prettier" },
+			typescriptreact = { "prettier" },
+			vue = { "prettier" },
+			css = { "prettier" },
+			scss = { "prettier" },
+			less = { "prettier" },
+			html = { "prettier" },
+			json = { "prettier" },
+			jsonc = { "prettier" },
+			yaml = { "prettier" },
+			markdown = { "prettier" },
+			graphql = { "prettier" },
+
+			-- System languages
+			lua = { "stylua" },
+			python = { "isort", "black" },
+			rust = { "rustfmt" },
+			go = { "goimports", "gofmt" },
+			c = { "clang_format" },
+			cpp = { "clang_format" },
+
+			-- Shell/config
+			sh = { "shfmt" },
+			bash = { "shfmt" },
+			zsh = { "shfmt" },
+
+			-- Fallback
+			["_"] = { "trim_whitespace" },
 		},
 	},
 }
