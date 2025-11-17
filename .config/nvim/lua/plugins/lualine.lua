94886a39e58fb37efcbaf69c0efbbadeee5bc843
Andrew Ward <asward@ucdavis.edu>
2025-11-17 07:55:12 -0800

    lualine for macros

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -12,7 +12,13 @@ return {
         lualine_c = {
           'filename',
         },
-        lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_x = {
+          {
+            require("noice").api.statusline.mode.get,
+            cond = require("noice").api.statusline.mode.has,
+            color = { fg = "#ff9e64" },
+          }, { 'encoding', 'fileformat', 'filetype' }
+        },
         lualine_y = { 'progress' },
         lualine_z = { 'location' }
       },
